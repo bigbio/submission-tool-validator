2a8da952da8cc9828164447851c8036630e564f7
Suresh Hewapathirana <sureshhewabi@gmail.com>
2021-01-28 17:34:54 +0000

    more validation added

--- src/main/java/uk/ac/ebi/pride/toolsuite/px_validator/utils/ResultReport.java
+++ src/main/java/uk/ac/ebi/pride/toolsuite/px_validator/utils/ResultReport.java
@@ -5,10 +5,13 @@ package uk.ac.ebi.pride.toolsuite.px_validator.utils;
  */
 public class ResultReport extends Report {
 
+    private String assayFile;
+    private long fileSize;
     private int numberOfProteins;
     private int numberOfPeptides;
     private int numberOfPSMs;
     private int numberOfPeakFiles;
+    private boolean isValidSchema = false;
 
     public ResultReport() {
 
@@ -30,11 +33,28 @@ public class ResultReport extends Report {
         this.numberOfPeakFiles = numberOfPeakFiles;
     }
 
+    public void setValidSchema(boolean validSchema) {
+        isValidSchema = validSchema;
+    }
+
+    public void setAssayFile(String assayFile) {
+        this.assayFile = assayFile;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
     @Override
     public String toString() {
-        return super.toString() + "Number of reported proteins : " + numberOfProteins + "\n" +
+        return super.toString() +
+                "Assay file : " + assayFile + "\n" +
+                "Assay file size: " + fileSize + "\n" +
+                "Valid Schema : " + isValidSchema + "\n" +
+                "Number of reported proteins : " + numberOfProteins + "\n" +
                 "Number of reported peptides : " + numberOfPeptides + "\n" +
                 "Number of reported PSMs : " + numberOfPSMs + "\n" +
                 "Number of peak files : " + numberOfPeakFiles + "\n";
+
     }
 }
